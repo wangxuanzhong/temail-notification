8767a59a578fc7800ecd1b869c9c280d4d5a6310
刘森 <liusen@syswin.com>
2018-09-27 15:56:05 +0800

    ZJCB-216 修改rocketmq-client日志级别为error

--- notification-main/src/main/java/com/syswin/temail/notification/main/Application.java
+++ notification-main/src/main/java/com/syswin/temail/notification/main/Application.java
@@ -9,6 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
   public static void main(String[] args) {
+
+    System.setProperty("rocketmq.client.logLevel", "ERROR");
+//    System.setProperty("rocketmq.client.logUseSlf4j", "true");
+
     SpringApplication.run(Application.class, args);
   }
 }
