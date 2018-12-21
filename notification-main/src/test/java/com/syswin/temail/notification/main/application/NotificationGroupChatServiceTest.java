1fc246499014582eea52584cb2bac9667195f57e
刘森 <liusen@syswin.com>
2018-12-21 13:12:09 +0800

    修改测试使用h2数据库

--- notification-main/src/test/java/com/syswin/temail/notification/main/application/NotificationGroupChatServiceTest.java
+++ notification-main/src/test/java/com/syswin/temail/notification/main/application/NotificationGroupChatServiceTest.java
@@ -11,10 +11,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("h2")
 public class NotificationGroupChatServiceTest {
 
   private final String TEST_GROUP = "g";
