628072d62f3d7c3a1b386912235dd45764cf6a24
胡婷 <huting@syswin.com>
2019-03-13 14:46:49 +0800

    1. 多个redisTemplate添加bean名称区分，注入时，指定名称； 2. 统一swagger配置到main模块； 3. 统一mybatis配置到main模块； 4. httpdns二级缓存，改成可配置形式。

--- notification-main/src/main/java/com/syswin/temail/notification/main/configuration/NotificationSwagger2.java
+++ notification-main/src/main/java/com/syswin/temail/notification/main/configuration/NotificationSwagger2.java
@@ -1,17 +1,15 @@
 package com.syswin.temail.notification.main.configuration;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
-@EnableSwagger2
+//@Configuration
+//@EnableSwagger2
 public class NotificationSwagger2 {
 
   @Bean
@@ -20,7 +18,7 @@ public class NotificationSwagger2 {
         .apiInfo(apiInfo())
         .groupName("temail-notification")
         .select()
-        .apis(RequestHandlerSelectors.basePackage("com.syswin"))
+        .apis(RequestHandlerSelectors.basePackage("com.syswin.temail.notification.main"))
         .paths(PathSelectors.any())
         .build();
   }
