7ef119d91344f082393410921cb1983ac0b94264
胡婷 <huting@syswin.com>
2019-03-14 14:30:37 +0800

    revert notification swagger config

--- notification-main/src/main/java/com/syswin/temail/notification/main/configuration/NotificationMybatisConfiguration.java
+++ notification-main/src/main/java/com/syswin/temail/notification/main/configuration/NotificationMybatisConfiguration.java
@@ -1,7 +1,10 @@
 package com.syswin.temail.notification.main.configuration;
 
-//@Configuration
-//@MapperScan("com.syswin.temail.notification.main.infrastructure")
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.syswin.temail.notification.main.infrastructure")
 public class NotificationMybatisConfiguration {
 
 }
