47e7d7f7eaed4c8a20670d4d1bbb10b5b89aa773
胡婷 <huting@syswin.com>
2019-05-10 16:10:29 +0800

    恢复启动类ComponentScan基础包。

--- notification-main/src/main/java/com/syswin/temail/notification/main/Application.java
+++ notification-main/src/main/java/com/syswin/temail/notification/main/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = "com.syswin.temail")
+@SpringBootApplication(scanBasePackages = "com.syswin.temail.notification")
 public class Application extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
