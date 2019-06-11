7b52904203325863c1c9e19198ca7d7088ddec6c
胡婷 <huting@syswin.com>
2019-06-11 14:26:20 +0800

    添加javadoc

--- notification-foundation/src/main/java/com/syswin/temail/notification/foundation/application/IMqProducer.java
+++ notification-foundation/src/main/java/com/syswin/temail/notification/foundation/application/IMqProducer.java
@@ -1,15 +1,43 @@
 package com.syswin.temail.notification.foundation.application;
 
+/**
+ * @author 刘森
+ */
 public interface IMqProducer {
 
+  /**
+   * 启动
+   */
   void start();
 
+  /**
+   * 发消息
+   *
+   * @param body 内容
+   * @param topic 队列topic
+   * @param tags 标签
+   * @param keys ‘KEYS’属性值
+   */
   void sendMessage(String body, String topic, String tags, String keys);
 
+  /**
+   * 发消息
+   *
+   * @param body 内容
+   * @param tags 标签
+   */
   void sendMessage(String body, String tags);
 
+  /**
+   * 发消息
+   *
+   * @param body 内容
+   */
   void sendMessage(String body);
 
+  /**
+   * 关闭资源
+   */
   void stop();
 
 }
--- notification-foundation/src/main/java/com/syswin/temail/notification/foundation/application/ISequenceService.java
+++ notification-foundation/src/main/java/com/syswin/temail/notification/foundation/application/ISequenceService.java
@@ -1,5 +1,8 @@
 package com.syswin.temail.notification.foundation.application;
 
+/**
+ * @author 刘森
+ */
 public interface ISequenceService {
 
   long STEP = 1L;
@@ -8,6 +11,9 @@ public interface ISequenceService {
 
   /**
    * 获取下个序号
+   *
+   * @param key 键
+   * @return 序列号
    */
   Long getNextSeq(String key);
 
